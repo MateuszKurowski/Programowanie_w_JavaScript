9bde7e74bd18c80eb4cc2925f5ec887465839753
Mateusz Kurowski <mateusz.kurowski@microsoft.wsei.edu.pl>
2022-12-11 13:40:19 +0100

    Lab6

---
+++ lab6/script.js
@@ -0,0 +1,16 @@
+let canvas = document.getElementById('canvas')
+let ctx = canvas.getctx('2d')
+
+window_width = window.innerWidth
+window_height = window.innerHeight
+
+canvas.width = window_width
+canvas.height = window_height
+
+ctx.arc()
+tx.beginPath()
+ctx.moveTo()
+ctx.lineTo()
+ctx.fill()
+ctx.stroke()
+// ctx.clearRect()

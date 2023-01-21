6c32011f78744b76e041dbd3805ef18b8c031319
Mateusz Kurowski <mateusz.kurowski@microsoft.wsei.edu.pl>
2023-01-21 23:37:12 +0100

    lab7 sledzik

--- lab7/script.js
+++ lab7/script.js
@@ -3,6 +3,30 @@ const weatherArray = []
 
 const displayWeather = data => {
 	const { name } = data
+	if (weatherArray.includes(name.toLowerCase())) {
+		alert('This city has already been added!')
+		return
+	}
+
+	const { icon, description } = data.weather[0]
+	const { temp, humidity } = data.main
+	const weatherDiv = ` <div id="${name.toLowerCase()}" class="weather loading">
+    <h1 class="city">Weather in ${name}</h1>
+    <h2 class="temp">${temp} °C </h2>
+    <div class="flex">
+      <img src="http://openweathermap.org/img/wn/${icon}.png" alt="" class="icon" />
+      
+      <p class="description">${description}</p>
+    </div>
+    <p class="humidity">Humidity: ${humidity}% </p>
+  </div> `
+
+	if (weatherArray.length > 9) {
+		const deleted = weatherArray.shift()
+		document.querySelector('#' + deleted).remove()
+	}
+
+	document.querySelector('#weathers').innerHTML = weatherDiv + document.querySelector('#weathers').innerHTML
 
 	weatherArray.push(name.toLowerCase())
 
@@ -14,3 +38,21 @@ const downloadWeatherFromApi = city => {
 		.then(response => response.json())
 		.then(data => displayWeather(data))
 }
+
+const getCity = () => {
+	const searchBar = document.querySelector('.search-bar').value
+	downloadWeatherFromApi(searchBar)
+}
+
+document.querySelector('.search button').addEventListener('click', function () {
+	getCity()
+})
+
+document.querySelector('.search-bar').addEventListener('keyup', function (event) {
+	if (event.key == 'Enter') getCity()
+})
+
+const weathersSaved = JSON.parse(localStorage.getItem('weathers') || '[]')
+weathersSaved.forEach(element => {
+	downloadWeatherFromApi(element)
+})

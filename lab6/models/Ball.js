75ea88dd04af503a96d85bb6ed86bedaeef475ae
Mateusz Kurowski <mateusz.kurowski@microsoft.wsei.edu.pl>
2023-02-07 16:50:53 +0100

    Canvas - ocena 3 i 5 bez 4

---
+++ lab6/models/Ball.js
@@ -0,0 +1,119 @@
+const maxSpeed = 8
+const minSpeed = 0.3
+
+export class Ball {
+	constructor(canvasWidth, canvasHeight, context) {
+		this.canvasHeight = canvasHeight
+		this.canvasWidth = canvasWidth
+		this.context = context
+
+		this.minRadius = Math.min(canvasHeight, canvasWidth) * 0.1 * 0.2 //10%
+		this.maxRadius = 0
+		this.checkMaxRadius()
+		this.radius = Math.random() * (this.maxRadius - this.minRadius) + this.minRadius
+
+		this.speed = 0
+		this.vx = 0
+		this.vy = 0
+		this.setSpeed()
+		this.directionX = Math.floor((this.vx * 100) / this.speed)
+		this.directionY = Math.ceil((this.vy * 100) / this.speed)
+
+		this.color =
+			'rgb(' +
+			Math.floor(Math.random() * 256) +
+			',' +
+			Math.floor(Math.random() * 256) +
+			',' +
+			Math.floor(Math.random() * 256) +
+			')'
+
+		this.xPosition =
+			Math.random() * (Math.floor(this.canvasWidth) - Math.ceil(this.radius * 2)) + Math.ceil(this.radius)
+		this.yPosition = Math.floor(
+			Math.random() * (Math.floor(this.canvasHeight) - Math.ceil(this.radius * 2)) + Math.ceil(this.radius)
+		)
+		this.Sx = this.xPosition - this.radius
+		this.Sy = this.yPosition
+	}
+
+	draw(width, height) {
+		this.xPosition += this.vx
+		this.yPosition += this.vy
+
+		if (this.xPosition > width - this.radius || this.xPosition < this.radius) {
+			this.vx = -this.vx
+		}
+		if (this.yPosition > height - this.radius || this.yPosition < this.radius) {
+			this.vy = -this.vy
+		}
+		if (this.xPosition >= width - this.radius) {
+			this.xPosition = width - this.radius
+		}
+		if (this.yPosition >= height - this.radius) {
+			this.yPosition = height - this.radius
+		}
+		if (this.xPosition < 0 + this.radius) {
+			this.xPosition = this.radius
+		}
+		if (this.yPosition < 0 + this.radius) {
+			this.yPosition = this.radius
+		}
+
+		this.context.beginPath()
+		this.context.arc(this.xPosition, this.yPosition, this.radius, 0, 2 * Math.PI)
+		this.context.fillStyle = this.color
+		this.context.fill()
+		this.context.stroke()
+		this.context.closePath()
+	}
+
+	getPosition() {
+		return [this.xPosition, this.yPosition]
+	}
+
+	transferPower(powerToChange, canvasHeight, canvasWidth) {
+		this.canvasHeight = canvasHeight
+		this.canvasWidth = canvasWidth
+		const procent = (powerToChange * 100) / this.radius
+		this.radius += powerToChange
+		this.speed -= this.speed * ((procent * 1.1) / 100)
+		if (this.speed < minSpeed && this.speed > 0) {
+			this.speed = minSpeed
+		} else if (this.speed > -minSpeed && this.speed < 0) {
+			this.speed = -minSpeed
+		}
+		if (this.speed > maxSpeed) {
+			this.speed = maxSpeed
+		} else if (this.speed < -maxSpeed) {
+			this.speed = -maxSpeed
+		}
+
+		this.changeSpeed()
+	}
+
+	setSpeed() {
+		this.checkMaxRadius()
+		const procent = 100 - (this.radius * 100) / this.maxRadius
+		this.speed = procent / 10
+		const tempVx = this.speed * Math.random()
+		const tempVy = this.speed - tempVx
+		if (!this.vx && !this.vy) {
+			this.vx = tempVx
+			this.vy = tempVy
+			return
+		}
+	}
+
+	changeSpeed() {
+		const tempVx = this.speed * (this.directionX / 100)
+		const tempVy = this.speed * (this.directionY / 100)
+
+		this.vx = this.vx > 0 ? Math.abs(tempVx) : Math.abs(tempVx) * -1
+		this.vy = this.vy > 0 ? Math.abs(tempVy) : Math.abs(tempVy) * -1
+	}
+
+	checkMaxRadius() {
+		this.maxRadius = Math.min(this.canvasHeight, this.canvasWidth) * 0.2 * 0.4 // 80%
+	}
+}
--- lab6/script.js
+++ lab6/script.js
@@ -1,10 +1,11 @@
 import { Background } from './models/Background.js'
-import { Circle } from './models/Circle.js'
+import { Ball } from './models/Ball.js'
 
-let amountOfCircles = document.querySelector('.circles-amount')
+let amountOfBalls = document.querySelector('.circles-amount')
 const startStopButton = document.querySelector('.start-stop-button')
 const resetButton = document.querySelector('.reset-button')
 let isStarted = true
+let animationId
 
 const canvasBackground = document.getElementById('canvas-background')
 const canvasPlayground = document.getElementById('canvas-playground')
@@ -23,36 +24,25 @@ const setCanvasDimensions = () => {
 const background = new Background(backgroundCtx, getWidth(), 40)
 const circles = []
 
-function drawCircles() {
-	if (!isStarted) {
-		return
-	}
-	window.requestAnimationFrame(drawCircles)
-	background.draw()
-	playgroundCtx.clearRect(0, 0, getWidth(), getHeight())
-	circles.forEach(circle => circle.draw(getWidth(), getHeight()))
-}
-
-const createCircles = amount => {
+const createBalls = amount => {
 	for (let i = 0; i < amount; i++) {
-		circles.push(new Circle(getWidth(), getHeight(), playgroundCtx))
+		circles.push(new Ball(getWidth(), getHeight(), playgroundCtx))
 	}
 }
 
-const addOrDeleteCircles = () => {
-	console.log('addOrDeleteCircles')
-	amountOfCircles = document.querySelector('.circles-amount')
-	const amount = amountOfCircles.value
+const addOrDeleteBalls = () => {
+	amountOfBalls = document.querySelector('.circles-amount')
+	const amount = amountOfBalls.value
 
 	if (!amount) {
 		circles.splice(0, circles.length)
 	} else if (amount < 0) {
-		amountOfCircles.value = 0
+		amountOfBalls.value = 0
 		return
 	} else if (amount == circles.length) {
 		return
 	} else if (amount > circles.length) {
-		createCircles(amount - circles.length)
+		createBalls(amount - circles.length)
 	} else {
 		circles.splice(0, circles.length - amount)
 	}
@@ -62,7 +52,7 @@ const startStop = () => {
 	isStarted = !isStarted
 	if (isStarted) {
 		startStopButton.textContent = 'Stop'
-		drawCircles()
+		drawBalls()
 	} else {
 		startStopButton.textContent = 'Start'
 	}
@@ -70,29 +60,103 @@ const startStop = () => {
 
 const reset = () => {
 	circles.splice(0, circles.length)
-	createCircles(amountOfCircles.defaultValue)
-	amountOfCircles.value = amountOfCircles.defaultValue
+	playgroundCtx.clearRect(0, 0, getWidth(), getHeight())
+	backgroundCtx.clearRect(0, 0, getWidth(), getHeight())
+	backgroundCtx.reset()
+	playgroundCtx.reset()
+	setCanvasDimensions()
+	background.draw()
+	createBalls(amountOfBalls.defaultValue)
+	amountOfBalls.value = amountOfBalls.defaultValue
 	isStarted = true
-	drawCircles()
+	window.cancelAnimationFrame(animationId)
+	animationId = window.requestAnimationFrame(drawBalls)
 }
+
+const getDistance = (distanceI, distanceJ) => {
+	const x1 = distanceI[0]
+	const y1 = distanceI[1]
+	const x2 = distanceJ[0]
+	const y2 = distanceJ[1]
+
+	const x = Math.pow(x2 - x1, 2)
+	const y = Math.pow(y2 - y1, 2)
+	return Math.sqrt(x + y)
+}
+
+const transferPower = (firstBall, secondBall) => {
+	const power = 0.02
+	if (firstBall.radius > secondBall.radius) {
+		firstBall.transferPower(power, getWidth(), getHeight())
+		secondBall.transferPower(-power, getWidth(), getHeight())
+	} else if (firstBall.radius < secondBall.radius) {
+		firstBall.transferPower(-power, getWidth(), getHeight())
+		secondBall.transferPower(power, getWidth(), getHeight())
+	} else {
+		if (Math.round(Math.random()) == 0) {
+			firstBall.transferPower(power, getWidth(), getHeight())
+			secondBall.transferPower(-power, getWidth(), getHeight())
+		} else {
+			firstBall.transferPower(-power, getWidth(), getHeight())
+			secondBall.transferPower(power, getWidth(), getHeight())
+		}
+	}
+}
+
+const drawLine = (firstCirlePosition, secondCirlePosition) => {
+	playgroundCtx.beginPath()
+	playgroundCtx.strokeStyle = 'white'
+	playgroundCtx.moveTo(firstCirlePosition[0], firstCirlePosition[1])
+	playgroundCtx.lineTo(secondCirlePosition[0], secondCirlePosition[1])
+	playgroundCtx.stroke()
+	playgroundCtx.closePath()
+	playgroundCtx.strokeStyle = 'black'
+}
+
+function drawBalls() {
+	if (!isStarted) {
+		return
+	}
+	playgroundCtx.clearRect(0, 0, getWidth(), getHeight())
+	for (let i = 0; i < circles.length; i++) {
+		const circle = circles[i]
+		circle.draw(getWidth(), getHeight())
+		if (circle.radius < 5) {
+			circles.splice(i, 1)
+			continue
+		}
+		for (let j = i; j < circles.length; j++) {
+			if (i == j) {
+				continue
+			}
+			const firstCirle = circles[i]
+			const secondBall = circles[j]
+			try {
+				const distance =
+					getDistance(firstCirle.getPosition(), secondBall.getPosition()) - firstCirle.radius + secondBall.radius
+				if (distance < 200) {
+					drawLine(firstCirle.getPosition(), secondBall.getPosition())
+					transferPower(firstCirle, secondBall)
+				}
+			} catch (error) {}
+		}
+	}
+	animationId = window.requestAnimationFrame(drawBalls)
+}
+
+const divedBall = (firstBall, secondBall) => {
+	
+}
+
+setCanvasDimensions()
+background.draw()
+createBalls(amountOfBalls.value)
+animationId = window.requestAnimationFrame(drawBalls)
 
 window.addEventListener('resize', () => {
 	setCanvasDimensions()
 	background.draw()
-	for (let i = 0; i < circles.length; i++) {
-		const circle = circles[i]
-		circle.draw(getWidth(), getHeight())
-		if (circle.radius < 1) {
-			circles.splice(i, 1)
-		}
-	}
 })
-
-createCircles(amountOfCircles.value)
-setCanvasDimensions()
-background.draw()
-window.requestAnimationFrame(drawCircles)
-
-amountOfCircles.addEventListener('change', addOrDeleteCircles)
+amountOfBalls.addEventListener('change', addOrDeleteBalls)
 startStopButton.addEventListener('click', startStop)
 resetButton.addEventListener('click', reset)
